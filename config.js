d508fce3af05d25805fdc10b8bfe361b2363eff9
Alby Bott <albybott@gmail.com>
2019-03-26 20:18:08 +1300

    Add endpoint env check

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 
 // GraphQL server url yoga.dokku.nmowhanau.nz
-export const endpoint = `http://localhost:4444`;
-// export const endpoint = `https://yoga.dokku.nmowhanau.nz`;
+export const endpoint =
+  process.env.NODE_ENV === "production"
+    ? `https://yoga.dokku.nmowhanau.nz`
+    : `http://localhost:4444`;

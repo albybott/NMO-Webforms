4b5f74708a551f90f1d96769a6129fbb4170dd06
Alby Bott <albybott@gmail.com>
2019-03-26 14:40:03 +1300

    Add live yoga url

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 
-// GraphQL server url
-export const endpoint = `http://localhost:4444`;
+// GraphQL server url yoga.dokku.nmowhanau.nz
+// export const endpoint = `http://localhost:4444`;
+export const endpoint = `https://yoga.dokku.nmowhanau.nz`;
